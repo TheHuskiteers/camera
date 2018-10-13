de52b39f3acf9db6f7db4ef840f10830401dbb28
Jacob Dillon <jepityr@sdf.org>
2018-10-13 02:36:57 -0400

    Added video streaming

--- src/main.py
+++ src/main.py
@@ -3,35 +3,38 @@ import numpy as np
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
+import process
 
 # Setup camera
 camera = PiCamera()
-rawCapture = PiRGBArray(camera)
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
 # Let camera warmup
 time.sleep(0.1)
 
-# Capture image from camera
-camera.capture(rawCapture, format="bgr")
-image = rawCapture.array
-
-# Convert image to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
 # Init face_cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# Detect faces in the image
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+# Capture frames from camera
+for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
+    # Grab raw array representing the image
+    image = frame.array
 
-# largest_face = (0, 0, 0, 0)
-# for (x, y, w, h) in faces:
-#     if w * h > largest_face[2] * largest_face[3]:
-#         largest_face = (x, y, w, h)
+    # Convert frame to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Take the first face and draw a square around it
-(x, y, w, h) = faces[0]
-cv2.rectangle(image, (x, y),(x+w, y+h), (0, 255, 0), 2)
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-# Output image to file
-cv2.imwrite("output_faces.jpg", image)
+    # Take the first face and draw a square around it
+    if len(faces) != 0:
+        (x, y, w, h) = faces[0]
+        cv2.rectangle(image, (x, y),(x+w, y+h), (0, 255, 0), 2)
+    
+    # Show the frame
+    cv2.imshow("Frame", image)
+
+    # Clear the stream in prep for next frame
+    rawCapture.truncate(0)
